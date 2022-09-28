1e435745366a8967684060b1a46cd179f5b9f87f
Daniel Steigerwald <daniel@steigerwald.cz>
2022-09-28 14:19:50 +0200

    Add test server in config

--- packages/evolu/src/config.ts
+++ packages/evolu/src/config.ts
@@ -2,7 +2,9 @@ import { Config } from "./types.js";
 
 // eslint-disable-next-line functional/no-let
 export let config: Config = {
-  syncUrl: "http://localhost:4000",
+  // For local dev with the monorepo:
+  // config.syncUrl = "http://localhost:4000"
+  syncUrl: "https://bold-frost-4029.fly.dev",
   log: false,
   maxDrift: 60000,
 };
