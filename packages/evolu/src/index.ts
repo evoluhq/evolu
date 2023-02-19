31a36e8547d506801b8865ff4784cb0c6e93e211
Daniel Steigerwald <daniel@steigerwald.cz>
2023-02-19 22:35:06 +0100

    Remove internals from public API
    If necessary, we will reexport them namespaced.

--- packages/evolu/src/index.ts
+++ packages/evolu/src/index.ts
@@ -1,18 +1,2 @@
-export * from "./createEvolu.js";
 export * from "./createHooks.js";
-export * from "./merkleTree.js";
 export * from "./model.js";
-export * as model from "./model.js";
-export * from "./perf.js";
-export {
-  CrdtMessageContent,
-  EncryptedCrdtMessage,
-  SyncRequest,
-  SyncResponse,
-} from "./protobuf.js";
-export {
-  createSyncTimestamp,
-  timestampFromString,
-  timestampToString,
-} from "./timestamp.js";
-export * from "./types.js";
