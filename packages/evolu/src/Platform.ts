33bd2c3d2182216664031c29ee6382545bb91986
Daniel Steigerwald <daniel@steigerwald.cz>
2023-08-12 12:56:55 +0200

    Refactor SyncLock

--- packages/evolu/src/Platform.ts
+++ packages/evolu/src/Platform.ts
@@ -5,13 +5,8 @@ export type FlushSync = (callback: () => void) => void;
 export const FlushSync = Context.Tag<FlushSync>("evolu/FlushSync");
 
 export interface SyncLock {
-  // acquire
-  // release
-
-  readonly isSyncing: Effect.Effect<never, never, boolean>;
-  readonly setIsSyncing: (
-    isSyncing: boolean
-  ) => Effect.Effect<never, never, void>;
+  readonly acquire: Effect.Effect<never, never, boolean>;
+  readonly release: Effect.Effect<never, never, void>;
 }
 
 export const SyncLock = Context.Tag<SyncLock>("evolu/SyncLock");
--- packages/evolu/src/Platform.web.ts
+++ packages/evolu/src/Platform.web.ts
@@ -2,47 +2,43 @@ import { Effect, Layer, Predicate, ReadonlyArray } from "effect";
 import { flushSync } from "react-dom";
 import { FlushSync, SyncLock } from "./Platform.js";
 
-export const FlushSyncLive = Layer.succeed(FlushSync, FlushSync.of(flushSync));
+export const FlushSyncLive = Layer.succeed(FlushSync, flushSync);
 
-const syncLockName = "evolu:sync";
-
-const hasLock: Predicate.Predicate<LockInfo[] | undefined> = (lockInfos) => {
-  if (lockInfos == null) return false;
-  return ReadonlyArray.some(
-    lockInfos,
-    (lockInfo) => lockInfo.name === syncLockName
-  );
-};
-
-const isSyncing: SyncLock["isSyncing"] = Effect.promise(() =>
-  navigator.locks.query()
-).pipe(Effect.map(({ pending, held }) => hasLock(pending) || hasLock(held)));
-
-let isSyncingResolve: null | ((value: null) => void) = null;
-
-const setIsSyncing: SyncLock["setIsSyncing"] = (isSyncing) =>
+export const SyncLockLive = Layer.effect(
+  SyncLock,
   Effect.sync(() => {
-    if (isSyncing) {
-      if (isSyncingResolve) return;
-      const promise = new Promise<null>((resolve) => {
+    const syncLockName = "evolu:sync";
+
+    const hasLock: Predicate.Predicate<LockInfo[] | undefined> = (
+      lockInfos
+    ) => {
+      if (lockInfos == null) return false;
+      return ReadonlyArray.some(
+        lockInfos,
+        (lockInfo) => lockInfo.name === syncLockName
+      );
+    };
+
+    const isSyncing = Effect.promise(() => navigator.locks.query()).pipe(
+      Effect.map(({ pending, held }) => hasLock(pending) || hasLock(held))
+    );
+
+    let isSyncingResolve: null | ((value: undefined) => void) = null;
+
+    const acquire: SyncLock["acquire"] = Effect.gen(function* (_) {
+      if (isSyncingResolve || (yield* _(isSyncing))) return false;
+      const promise = new Promise<undefined>((resolve) => {
         isSyncingResolve = resolve;
       });
-      // ok, co se stane v jinem tabu?
-      // isSyncing by to tam nemelo pustit, to je async, ok
-      // ale co kdyz to nastavim? imho blbe api, ne?
-      // nemelo by stacit request a release?
-      // co se stane, kdyz to nahodou v jinem tabu zavolam?
-      // will be queued, jasny, coz ale nechci imho
+      void navigator.locks.request(syncLockName, () => promise);
+      return true;
+    });
 
-      // eslint-disable-next-line @typescript-eslint/no-floating-promises
-      navigator.locks.request(syncLockName, () => promise);
-    } else {
-      if (isSyncingResolve) isSyncingResolve(null);
+    const release: SyncLock["release"] = Effect.sync(() => {
+      if (isSyncingResolve) isSyncingResolve(undefined);
       isSyncingResolve = null;
-    }
-  });
+    });
 
-export const SyncLockLive = Layer.succeed(SyncLock, {
-  isSyncing,
-  setIsSyncing,
-});
+    return { acquire, release };
+  })
+);
