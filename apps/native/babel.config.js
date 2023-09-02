46f033f34546338b175735ad07b98d861fc1ef29
Daniel Steigerwald <daniel@steigerwald.cz>
2023-09-02 12:09:22 +0200

    Purge and reorganize deps

--- apps/native/babel.config.js
+++ apps/native/babel.config.js
@@ -10,13 +10,7 @@ module.exports = function (api) {
           alias: {
             // TODO: Use react-native-quick-crypto once fixed for RN 0.72
             // 'crypto': 'react-native-quick-crypto',
-            // 'node:crypto': 'react-native-quick-crypto',
             crypto: "crypto-browserify",
-            "node:crypto": "crypto-browserify",
-            // TODO: Do we still need stream and buffer?
-            stream: "stream-browserify",
-            buffer: "@craftzdog/react-native-buffer",
-            "node:buffer": "@craftzdog/react-native-buffer",
           },
         },
       ],
