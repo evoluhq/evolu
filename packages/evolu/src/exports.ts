4b5c7f9363363ddc7785a155a2ea93e20ccf7fa9
Daniel Steigerwald <daniel@steigerwald.cz>
2023-09-22 22:17:12 +0200

    Export types Mnemonic, Owner, OwnerId

--- packages/evolu/src/exports.ts
+++ packages/evolu/src/exports.ts
@@ -1,4 +1,6 @@
 export { jsonArrayFrom, jsonObjectFrom } from "kysely/helpers/sqlite";
+export type { Mnemonic } from "./Crypto.js";
+export type { Owner, OwnerId } from "./Db.js";
 export type { EvoluError } from "./Errors.js";
 export * from "./Model.js";
 export type { SyncState } from "./SyncWorker.js";
