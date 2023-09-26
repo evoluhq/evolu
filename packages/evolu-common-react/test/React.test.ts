187119edb8f9defb3c21b95f48b83e2ac5404d42
Daniel Steigerwald <daniel@steigerwald.cz>
2023-09-26 15:46:30 +0200

    I hate when CI tests fail without any error

--- packages/evolu-common-react/test/React.test.ts
+++
@@ -1,6 +0,0 @@
-import { expect, test } from "vitest";
-
-test("ReactHooksLive", () => {
-  // TODO: Tests.
-  expect(true).toBeTruthy();
-});
--- packages/evolu-common-web/test/PlatformLive.test.ts
+++
@@ -1,7 +0,0 @@
-import { expect, test } from "vitest";
-import { AppStateLive } from "../src/PlatformLive.js";
-
-test("AppStateLive", () => {
-  // TODO: Tests.
-  expect(AppStateLive).toBeTruthy();
-});
