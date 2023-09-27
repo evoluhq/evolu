28354719ead6754cc1fbc21c32934bf6210394cb
Daniel Steigerwald <daniel@steigerwald.cz>
2023-09-27 14:32:40 +0200

    Remove deprecated evolu

--- packages/evolu/src/exports.ts
+++
@@ -1,6 +0,0 @@
-export { jsonArrayFrom, jsonObjectFrom } from "kysely/helpers/sqlite";
-export type { Mnemonic } from "./Crypto.js";
-export type { Owner, OwnerId } from "./Db.js";
-export type { EvoluError } from "./Errors.js";
-export * from "./Model.js";
-export type { SyncState } from "./SyncWorker.js";
