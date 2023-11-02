b45e135e6e07667ce8e1b335c5d13f167030082f
Daniel Steigerwald <daniel@steigerwald.cz>
2023-11-02 01:26:53 +0100

    Better public API re-export

--- packages/evolu-common/src/Public.ts
+++ packages/evolu-common/src/Public.ts
@@ -3,4 +3,5 @@ export type { Mnemonic } from "./Crypto.js";
 export type { Owner, OwnerId } from "./Db.js";
 export type { EvoluError } from "./Errors.js";
 export * from "./Model.js";
+export { canUseDom } from "./Platform.js";
 export type { SyncState } from "./SyncWorker.js";
--- packages/evolu-react/src/index.ts
+++ packages/evolu-react/src/index.ts
@@ -9,25 +9,7 @@ import {
 } from "@evolu/common-web";
 import { Layer } from "effect";
 
-// Public API. It must be copy-pasted to all Evolu UI libs because re-exporting
-// from @evolu/common/Public is not working for some reason.
-export {
-  Id,
-  NonEmptyString1000,
-  Owner,
-  PositiveInt,
-  SqliteBoolean,
-  SqliteDate,
-  String,
-  String1000,
-  canUseDom,
-  cast,
-  id,
-  jsonArrayFrom,
-  jsonObjectFrom,
-} from "@evolu/common";
-// Re-exporting a type when 'isolatedModules' is enabled requires using 'export type'.
-export type { EvoluError, Mnemonic, OwnerId, SyncState } from "@evolu/common";
+export * from "@evolu/common/public";
 
 export const create = makeReactHooksForPlatform(
   Layer.use(DbWorkerLive, PlatformLive),
