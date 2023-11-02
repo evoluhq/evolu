7db754a0807c6df389ac9f964e30e96503d32870
Daniel Steigerwald <daniel@steigerwald.cz>
2023-11-02 01:12:00 +0100

    Fix linting and type checking in next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,16 +1,21 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+// @ts-check
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/require-await */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+
+// @ts-expect-error Missing types.
 const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.jsx",
 });
 
-// eslint-disable-next-line jsdoc/valid-types
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     optimizePackageImports: ["effect"],
   },
-  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
   async headers() {
     return [
       {
