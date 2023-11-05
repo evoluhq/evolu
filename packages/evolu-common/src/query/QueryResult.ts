e3102cab81af3bde9910c5897d850709ba9173a0
Daniel Steigerwald <daniel@steigerwald.cz>
2023-11-05 23:36:57 +0100

    Rename makeQueryResult to queryResultFromRows

--- packages/evolu-common/src/query/QueryResult.ts
+++ packages/evolu-common/src/query/QueryResult.ts
@@ -6,6 +6,6 @@ export interface QueryResult<R extends Row> {
   readonly firstRow: Readonly<Kysely.Simplify<R>> | null;
 }
 
-export const makeQueryResult = <R extends Row>(
+export const queryResultFromRows = <R extends Row>(
   rows: ReadonlyArray<R>,
 ): QueryResult<R> => ({ rows, firstRow: rows[0] });
