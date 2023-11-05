3197507c161a844d1a9fb1b40773ed8e09faf5b9
Daniel Steigerwald <daniel@steigerwald.cz>
2023-11-05 23:27:49 +0100

    Add makeQueryResult

--- packages/evolu-common/src/query/QueryResult.ts
+++ packages/evolu-common/src/query/QueryResult.ts
@@ -5,3 +5,7 @@ export interface QueryResult<R extends Row> {
   readonly rows: ReadonlyArray<Readonly<Kysely.Simplify<R>>>;
   readonly firstRow: Readonly<Kysely.Simplify<R>> | null;
 }
+
+export const makeQueryResult = <R extends Row>(
+  rows: ReadonlyArray<R>,
+): QueryResult<R> => ({ rows, firstRow: rows[0] });
