62387a27029dbbf1e9ce166ff665d3c55a024405
Nedim Arabacı <nedimarabaci@gmail.com>
2025-06-12 03:14:25 +0300

    change nextjs example appname

--- apps/web/src/components/NextJsExample.tsx
+++ apps/web/src/components/NextJsExample.tsx
@@ -104,7 +104,7 @@ const formatTypeError = createFormatTypeError<
 
 const evolu = createEvolu(evoluReactWebDeps)(Schema, {
   reloadUrl: "/docs/examples/react/nextjs",
-  name: getOrThrow(SimpleName.from("evolu-nextjs-example")),
+  name: getOrThrow(SimpleName.from("evolu-nextjs-example-v120625")),
 
   ...(process.env.NODE_ENV === "development" && {
     syncUrl: "http://localhost:4000",
