3fc767578e2a35b3018f93d44451fd850d098d2a
trevorpe <t.a.profitt@gmail.com>
2025-07-17 21:05:21 -0300

    feat: add Vue example

---
+++ examples/vue-vite-pwa/src/App.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import PWABadge from './components/PWABadge.vue'
+</script>
+
+<template>
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="/favicon.svg" class="logo" alt="vue-vite-pwa logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="vue-vite-pwa" />
+  <PWABadge />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ examples/vue-vite-pwa/vite.config.ts
@@ -0,0 +1,39 @@
+import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: "prompt",
+      injectRegister: false,
+
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
+
+      manifest: {
+        name: "vue-vite-pwa",
+        short_name: "vue-vite-pwa",
+        description: "vue-vite-pwa",
+        theme_color: "#ffffff",
+      },
+
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+      },
+
+      devOptions: {
+        enabled: false,
+        navigateFallback: "index.html",
+        suppressWarnings: true,
+        type: "module",
+      },
+    }),
+  ],
+});
