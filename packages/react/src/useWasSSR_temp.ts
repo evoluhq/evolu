0f471c124047fd23314dc6f73202d9ce4846f8b4
Daniel Steigerwald <daniel@steigerwald.cz>
2025-09-24 11:40:38 +0200

    We need useWasSsr

--- packages/react/src/useWasSSR_temp.ts
+++ packages/react/src/useWasSSR_temp.ts
@@ -5,9 +5,14 @@ const emptySubscribe = () => () => {};
 
 /**
  * Unlike the `typeof window === 'undefined'`, this ensures that the server and
- * hydration sees the same thing.
+ * hydration see the same thing.
+ *
+ * We use this so people can use Evolu with SSR even though it's not recommended
+ * because of layout shift during rendering.
+ *
+ * @see https://tkdodo.eu/blog/avoiding-hydration-mismatches-with-use-sync-external-store
  */
-export const useWasSSR = (): boolean => {
+export const useWasSsr = (): boolean => {
   const ref = useRef(false);
   return useSyncExternalStore(
     emptySubscribe,
