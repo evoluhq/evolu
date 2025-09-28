2fa361dd41dcbe96ed65447f2f9dbbb6f0656bf3
Nedim Arabacı <nedimarabaci@gmail.com>
2025-09-28 16:01:49 +0300

    revive react-electron example

---
+++ examples/react-electron/vite.preload.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
+
+// https://vitejs.dev/config
+export default defineConfig({
+  optimizeDeps: {
+    exclude: [
+      "@sqlite.org/sqlite-wasm",
+      "kysely",
+      "@evolu/common",
+      "@evolu/react",
+      "@evolu/react-web",
+    ],
+  },
+  plugins: [nodePolyfills()],
+});
---
+++ examples/react-electron/vite.renderer.config.ts
@@ -0,0 +1,16 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
+// https://vitejs.dev/config
+export default defineConfig({
+  optimizeDeps: {
+    exclude: [
+      "@sqlite.org/sqlite-wasm",
+      "kysely",
+      "@evolu/common",
+      "@evolu/react",
+      "@evolu/react-web",
+    ],
+  },
+  plugins: [react(), nodePolyfills()],
+});
