7245d6ac911d035224f649644155050fb5f278fc
Syntax <2079305+TheUltDev@users.noreply.github.com>
2025-10-18 02:40:40 -0500

    fix: add coop headers

--- examples/react-vite-pwa/vite.config.ts
+++ examples/react-vite-pwa/vite.config.ts
@@ -39,5 +39,15 @@ export default defineConfig({
         type: "module",
       },
     }),
+    {
+      name: "configure-response-headers",
+      configureServer: (server) => {
+        server.middlewares.use((_req, res, next) => {
+          res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+          res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+          next();
+        });
+      },
+    },
   ],
 });
