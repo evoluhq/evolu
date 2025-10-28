bda06dd35e568fd50e5047fad44a9b832ae64020
Daniel Steigerwald <daniel@steigerwald.cz>
2025-10-28 08:58:57 +0100

    Rename EvoluProfilePic to EvoluOwnerIdIdenticon

--- packages/react-web/src/index.ts
+++ packages/react-web/src/index.ts
@@ -2,7 +2,7 @@ import { EvoluDeps } from "@evolu/common/evolu";
 import { evoluWebDeps } from "@evolu/web";
 import { flushSync } from "react-dom";
 
-export * from "./EvoluProfilePic.js";
+export * from "./EvoluOwnerIdIdenticon.js";
 
 export const evoluReactWebDeps: EvoluDeps = {
   ...evoluWebDeps,
