0132d7b19f60f0919470e42d6c0dcb20010c6987
Daniel Steigerwald <daniel@steigerwald.cz>
2025-10-30 10:37:34 +0100

    Fix CLI tests

--- packages/common/vitest.config.ts
+++ packages/common/vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    exclude: ["**/node_modules/**", "**/dist/**"],
     snapshotSerializers: ["./test/Evolu/_uint8ArraySerializer.ts"],
   },
 });
