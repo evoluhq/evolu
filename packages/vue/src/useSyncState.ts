bfe948d2f8039f3e29cdb05dddc85a07b9c313cf
Daniel Steigerwald <daniel@steigerwald.cz>
2025-11-15 21:43:45 +0100

    Remove unused imports in useSyncState.ts
    To fix eslint warning

--- packages/vue/src/useSyncState.ts
+++ packages/vue/src/useSyncState.ts
@@ -1,6 +1,6 @@
 import { SyncState } from "@evolu/common/evolu";
-import { useEvolu } from "./useEvolu.js";
-import { onScopeDispose, Ref, ref } from "vue";
+import { Ref } from "vue";
+// import { useEvolu } from "./useEvolu.js";
 
 /** Subscribe to {@link SyncState} changes. */
 export const useSyncState = (): Ref<SyncState> => {
