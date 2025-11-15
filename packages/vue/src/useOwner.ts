8e4a94939365629b5a8ec762164c4daa40dd39b4
arbyys <adam.petricek@centrum.cz>
2025-11-15 02:01:42 +0100

    Change composable name to match React package, remove unnecessary imports

--- packages/vue/src/useOwner.ts
+++ packages/vue/src/useOwner.ts
@@ -1,11 +1,10 @@
 import { SyncOwner } from "@evolu/common";
 import { useEvolu } from "./useEvolu.js";
-import { onScopeDispose, Ref, ref } from "vue";
 
 /** 
  * Vue composable for managing sync owner subscriptions.
  */
-export const useAppOwner = (owner: SyncOwner | null): void => {
+export const useOwner = (owner: SyncOwner | null): void => {
   if (owner == null) return;
 
   const evolu = useEvolu();
