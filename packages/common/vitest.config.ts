58f7d4ed47fedc3df4839d3d9079440d2d06f563
Daniel Steigerwald <daniel@steigerwald.cz>
2025-11-24 01:17:17 +0100

    Rename test/Evolu to test/local-first

--- packages/common/vitest.config.ts
+++ packages/common/vitest.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     exclude: ["**/node_modules/**", "**/dist/**"],
-    snapshotSerializers: ["./test/Evolu/_uint8ArraySerializer.ts"],
+    snapshotSerializers: ["./test/local-first/_uint8ArraySerializer.ts"],
   },
 });
