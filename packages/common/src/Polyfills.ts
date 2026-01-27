ec9a7ef53f439c6fb3a6e629d13bb5f96ab9839e
Daniel Steigerwald <daniel@steigerwald.cz>
2026-01-27 13:44:52 +0100

    Remove es-aggregate-error and its types from dependencies

--- packages/common/src/Polyfills.ts
+++ packages/common/src/Polyfills.ts
@@ -5,20 +5,15 @@
  */
 
 import "disposablestack/auto";
-import AggregateError from "es-aggregate-error";
+import { lazyVoid } from "./Function.js";
 
 /**
  * Installs polyfills for resource management.
  *
  * Polyfills `Symbol.dispose`, `Symbol.asyncDispose`, `DisposableStack`,
- * `AsyncDisposableStack`, `SuppressedError`, and `AggregateError`.
- *
- * Required for WebKit and React Native.
+ * `AsyncDisposableStack`, and `SuppressedError`.
  *
  * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Resource_management
  * @see https://github.com/es-shims/DisposableStack
- * @see https://github.com/es-shims/AggregateError
  */
-export const installPolyfills = (): void => {
-  AggregateError.shim();
-};
+export const installPolyfills = lazyVoid;
