6d368c2a428cc25975979dc9b566f9c8693305cd
Daniel Steigerwald <daniel@steigerwald.cz>
2026-02-02 23:24:43 +0100

    Remove re-export of @evolu/web from react-web

--- packages/react-web/src/index.ts
+++ packages/react-web/src/index.ts
@@ -2,7 +2,6 @@ import type { EvoluDeps } from "@evolu/common/local-first";
 import { createEvoluDeps as createWebEvoluDeps } from "@evolu/web";
 import { flushSync } from "react-dom";
 
-export * from "@evolu/web";
 export * from "./components/index.js";
 
 /** Creates Evolu dependencies for React web with React DOM flush sync support. */
